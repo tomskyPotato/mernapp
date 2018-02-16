1e959c4e803e460f8ba74efc58adbcce549f8d2a
unknown <tomsky@posteo.de>
2018-02-16 17:57:05 +0100

    änderungen um die app auf heroku zu deployen

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://user2:1234@ds125938.mlab.com:25938/reactjs',
+  mongoURLlocal: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
 
